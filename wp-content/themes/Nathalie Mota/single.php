017a393535254a5e4862221c645ec3236c2f2933
tabbrat <tabbrat@yahoo.fr>
2024-08-09 04:28:51 +0200

    finalisation css mise en forme page single.php et rajout de card photo ou avatar avec arrows right et left fonctionnels telechargement des arrows  DEL: dossier css et fichier  css single-photo d'essaie renettoyer css inutile pour le moment...

--- wp-content/themes/Nathalie Mota/single.php
+++ wp-content/themes/Nathalie Mota/single.php
@@ -2,9 +2,9 @@
 get_header();
 ?>
 
-<?php 
+<?php
 // Vérifier l'activation de ACF
-if ( !function_exists('get_field')) return;
+if (!function_exists('get_field')) return;
 
 // Récupérer la taxonomie actuelle
 $term = get_queried_object();
@@ -13,51 +13,91 @@ $term_id  = $term ? $term->term_id : null;
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php 
-// Récupération du nom de la catégorie et du format
-$Formats = get_field('format-acf') ? get_field('format-acf') : 'Inconnu';
-$reference = get_post_meta(get_the_ID(), 'reference', true);
-$type = get_field('type') ? get_field('type') : 'Inconnu'; 
-?>
- 
-<section class="container single_post">
-    <div class="post-info">
-        <!-- Affiche titre photo -->
-        <h2 class="single_title"><?php echo get_the_title(); ?></h2>
-        <!-- Affiche les données ACF -->
-        <div class="event-meta">
-            <ul>
-                <li>Référence : <?php echo $reference; ?></li>
-                <li>catégorie :<?php the_terms(get_the_ID(), 'categorie', false);?></li>
-                <li>format : <?php echo the_terms(get_the_ID(), 'format', false) ?></li>
-                <li>type : <?php echo $type; ?></li>
-                <li>année :<?php the_date('Y'); ?></li>
-            </ul>
-        </div>
-    </div>
-    <div class="post-image">
-        <?php the_post_thumbnail('custom-size'); ?>
-    </div>
-</section>  
+        <?php
+        // Récupération du nom de la catégorie et du format
+        $Formats = get_field('format-acf') ? get_field('format-acf') : 'Inconnu';
+        $reference = get_post_meta(get_the_ID(), 'reference', true);
+        $type = get_field('type') ? get_field('type') : 'Inconnu';
+        ?>
 
-<section class="contact_line">
-    <div class="contact-section">
-        <p>Cette photo vous intéresse ? </p>
-        <a href="mailto:example@example.com" class="contact-button">
-            Contact
-        </a>
-    </div>
-</section>
+        <section class="container single_post">
+            <div class="post-info">
+                <!-- Affiche titre photo -->
+                <h2 class="single_title"><?php echo get_the_title(); ?></h2>
+                <!-- Affiche les données ACF -->
+                <div class="event-meta">
+                    <ul>
+                        <li>Référence : <?php echo $reference; ?></li>
+                        <li>catégorie :<?php the_terms(get_the_ID(), 'categorie', false); ?></li>
+                        <li>format : <?php echo the_terms(get_the_ID(), 'format', false) ?></li>
+                        <li>type : <?php echo $type; ?></li>
+                        <li>année :<?php the_date('Y'); ?></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="post-image">
+                <?php the_post_thumbnail('custom-size'); ?>
+            </div>
+        </section>
 
-<?php endwhile; endif; ?>
+        <section class="contact_line">
+            <div class="contact-section">
+                <p>Cette photo vous intéresse ? </p>
+                <a href="mailto:example@example.com" class="contact-button">
+                    Contact
+                </a>
+                <div class="navigation-avatar">				
+				<div class="navigation-prev">
+				<?php
+					$prev_post = get_previous_post();							
+					if($prev_post) {
+						$prev_title = strip_tags(str_replace('"', '', $prev_post->post_title));
+						$prev_post_id = $prev_post->ID;
+						echo '<a rel="prev" href="' . get_permalink($prev_post_id) . '" title="' . $prev_title. '" class="previous_post">';
+						if (has_post_thumbnail($prev_post_id)){
+							?>
+							<div>
+								<?php echo get_the_post_thumbnail($prev_post_id, array(71,81));?></div>
+							<?php
+							}
+							else{
+							echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/no-image.jpeg" alt="Pas de photo" width="77px" ><br>';
+							}							
+							echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/precedent.png" alt="Photo précédente" ></a>';
+						}
+						?>
+				
+				<div class="navigation-next">
+					<!-- next_post_link( '%link', '%title', false );  -->
+					<?php
+						$next_post = get_next_post();
+						if($next_post) {
+							$next_title = strip_tags(str_replace('"', '', $next_post->post_title));
+							$next_post_id = $next_post->ID;
+							echo  '<a rel="next" href="' . get_permalink($next_post_id) . '" title="' . $next_title. '" class="next_post">';
+							if (has_post_thumbnail($next_post_id)){	
+							}
+							else{
+								echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/no-image.jpeg" alt="Pas de photo" width="77px" ><br>';
+							}							
+							echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/suivant.png" alt="Photo suivante" ></a>';
+						}
+					?>
+					</div>
+				</div>
+            </div>
+        </section>
+
+<?php endwhile;
+endif; ?>
 
 <?php
-$args = array (
-    'post_type' => 'photo', 
-    'posts_per_page' => 2, 
+$args = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 2,
     'orderby' => 'rand',
     "post__not_in" => array(get_the_ID()) // exclure la photo du post single.php
-);  
+);
 
 $query = new WP_Query($args);
 ?>
@@ -65,17 +105,16 @@ $query = new WP_Query($args);
 <section class="container_bas">
     <h3>VOUS AIMEREZ AUSSI</h3>
     <div class="img-bas">
-        <?php if( $query->have_posts() ) : while($query->have_posts()): $query->the_post(); ?>
-            <article class="cardb">
-                <img class="post-img" src="<?php echo get_the_post_thumbnail_url(); ?>" />
-            </article>
-        <?php endwhile; endif; wp_reset_postdata(); ?>
+        <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                <article class="cardb">
+                    <img class="post-img" src="<?php echo get_the_post_thumbnail_url(); ?>" />
+                </article>
+        <?php endwhile;
+        endif;
+        wp_reset_postdata(); ?>
     </div>
 </section>
 
 <?php
 get_footer();
 ?>
-
-
-
